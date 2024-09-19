c5e271ec1775a9dd95d671b98ed1ca0bbd543ff0
piyushlawatre <piyushlawatre.dev@gmail.com>
2024-09-19 12:37:51 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>Quizzer</div>;
+  return (
+    <div>
+      <h1>Quizzer</h1>
+    </div>
+  );
 }
 
 export default App;
